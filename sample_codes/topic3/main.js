b5a081896a75ad8e5101d7925aa0e123751ad901
Alfred Ang <angch@tertiaryinfotech.com>
2022-01-12 04:26:56 +0800

    init commit

---
+++ sample_codes/topic3/main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
+        }
+    }
+})
